d5bc9495620a6742f10f0eb2455a058bbeb3896e
Maeyanie <me@maeyanie.com>
2016-11-05 03:24:53 +0100

    Initial commit.

---
+++ benchmark.php
@@ -0,0 +1,89 @@
+#!/usr/bin/php
+<?php
+require "config.php";
+$bench = array();
+
+foreach ($algos as $id=>$algo) {
+	if ($algo === false) continue;
+	
+	echo "Benchmarking $algo...\n";
+	$count = 0;
+	$total = 0;
+	$first = true;
+
+	$cmd = "$algo --benchmark $args";
+	$ds = array(0 => array("pipe", "r"), 1 => array("pipe", "w"), 2 => array("pipe", "w"));
+	$pd = proc_open($cmd, $ds, $pipes);
+	if ($pd === false) {
+		echo "Execution failed.\n";
+		continue;
+	}
+	fclose($pipes[0]);
+	$timeouts = 0;
+	$line = "";
+	while (true) {
+		$read = array($pipes[1], $pipes[2]);
+		$c = stream_select($read, $write, $except, 30);
+		if ($c === false) break;
+		if ($c == 0) {
+			echo "Timeout $timeouts\n";
+			if ($timeouts++ > 10) break;
+			continue;
+		}
+		
+		//echo (isset($read[1])?"1":"-") . (isset($read[2])?"2":"-") . "\r";
+		$r = array_shift($read);
+		if (feof($r)) {
+			echo "Program exited.\n";
+			break;
+		}
+		$c = fgetc($r);
+		if ($c === false) continue;
+		$timeouts = 0;
+		$line .= $c;
+		if ($c != "\n") continue;
+		
+		echo "\t$line";
+		
+		// [2015-12-09 02:20:16] Total: 48.646 H/s
+		if (preg_match('|Total: ([\d\.]+) ([kMG]?H)/s|', $line, $matches)) {
+			if ($first) { $first = false; continue; }
+			echo "\t$matches[1] $matches[2]\n";
+			switch ($matches[2]) {
+			case 'GH': $matches[1] *= 1000;
+			case 'MH': $matches[1] *= 1000;
+			case 'kH': $matches[1] *= 1000;
+			}
+			$total += $matches[1];
+			if ($count++ == 5) break;
+		} else if (preg_match('|Total: ([\d\.]+) hash/s|', $line, $matches)) {
+			if ($first) { $first = false; continue; }
+			echo "\t$matches[1] H\n";
+			$total += $matches[1];
+			if ($count++ == 5) break;
+		} else if (preg_match("|Trial \d\.\.\. (\d+)|", $line, $matches)) {
+			$total += $matches[1];
+			echo "\t$matches[1] H\n";
+			if ($count++ == 5) break;
+		} else if (strpos($line, "FATAL") !== false || strpos($line, "Err") !== false) {
+			echo "\tError running benchmark.\n";
+			break;
+		}
+		$line = "";
+	}
+	fclose($pipes[1]);
+	fclose($pipes[2]);
+	unset($read);
+	unset($pipes);
+	proc_terminate($pd);
+	proc_close($pd);
+	
+	if ($count == 0) $avg = 0;
+	else $avg = $total / $count;
+	
+	echo "\tAverage: $avg H/s\n";
+	$bench[$id] = $avg;
+	sleep(5);
+}
+
+file_put_contents("benchmark.json", json_encode($bench));
---
+++ switcher.php
@@ -0,0 +1,81 @@
+#!/usr/bin/php
+<?php
+require "config.php";
+
+if (!isset($bench)) {
+	$bench = json_decode(file_get_contents("benchmark.json"), true);
+	if ($bench === null) die("No benchmarks available.");
+}
+
+$current = -1;
+$pd = false;
+$ds = array(0 => array("pipe", "r"), 1 => array("file", readlink("/dev/stdout"), "w"), 2 => array("file", readlink("/dev/stderr"), "w"));
+array_shift($argv);
+
+while (true) {
+	echo "Fetching coin info...\n";
+	$curl = curl_init("https://www.nicehash.com/api?method=simplemultialgo.info");
+	if (!$curl) die("curl_init failed.");
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	$data = curl_exec($curl);
+	curl_close($curl);
+	if ($data === false) {
+		echo "Error fetching data: ".curl_error($curl)."\n";
+		sleep(30 + mt_rand(0,6) - mt_rand(0,6));
+		continue;
+	}
+	$json = json_decode($data);
+	if (!isset($json->result) || !isset($json->result->simplemultialgo)) {
+		echo "Error decoding data: $data\n";
+		sleep(30 + mt_rand(0,6) - mt_rand(0,6));
+		continue;
+	}
+
+	$best = array();
+	foreach ($json->result->simplemultialgo as $a) {
+		if (!isset($algos[$a->algo])) { echo "*** New algorithm detected: $a->name ($a->algo) ***\n"; continue; }
+		if ($algos[$a->algo] === false) continue;
+		$id = $a->algo;
+		$name = $a->name;
+		$algo = $algos[$id];
+		//         BTC/GHps/day * KHps = uBTC/day
+		$pay = round($a->paying * ($bench[$id]/1000), 3);
+		if ($pay != 0) $best["$pay"] = array($id, $name, $algo, $pay);
+	}
+	krsort($best);
+	foreach ($best as $k=>$v) {
+		echo "$v[1] ($v[2]): $v[3] uBTC/day\n";
+	}
+	$info = array_values($best)[0];
+	
+	if ($info[0] != $current) {
+		if ($pd !== false) {
+			echo "Terminating running miner...\n";
+			proc_terminate($pd);
+			proc_close($pd);
+		}
+	
+		if ($info[0] == 999) { // Ethereum
+			$o = "http://ethereum.$region.nicehash.com:3500/n1c3-$btcaddr.$name/1";
+			$cmd = "$info[2] --farm $o";
+		} else if ($info[0] == 24) { // EquiHash
+			$cmd = "$info[2] -l $region -u $btcaddr $args";
+		} else if ($info[0] == 20) { // DaggerHashimoto
+			$o = "stratum+tcp://$info[1].$region.nicehash.com:".(3333+$info[0]);
+			$cmd = "$info[2] -S $o -O $btcaddr.$name:x $args ".implode(" ", $argv);
+		} else {
+			$o = "stratum+tcp://$info[1].$region.nicehash.com:".(3333+$info[0]);
+			$cmd = "$info[2] -o $o -O $btcaddr.$name:x $args ".implode(" ", $argv);
+		}
+	
+		echo "Running: $cmd\n";
+		$pd = proc_open($cmd, $ds, $pipes);
+		if ($pd === false) exit;
+		fclose($pipes[0]);
+		$current = $info[0];
+	}
+	
+	sleep(60 + mt_rand(0,10) - mt_rand(0,10));
+}
+
+?>
